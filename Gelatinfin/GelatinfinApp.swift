fee7b8df6cdff4df4c694a707c6845355e1ae489
Kevin Hamby <khamby@smithmicro.com>
2023-09-26 10:53:27 -0400

    Initial Commit

---
+++ Gelatinfin/GelatinfinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GelatinfinApp.swift
+//  Gelatinfin
+//
+//  Created by Kevin Hamby on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GelatinfinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
